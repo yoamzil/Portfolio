dba31d3f1daef48c8a4564b01d105765416018fd
Youness Amzil <amzilyouness@gmail.com>
2024-12-06 17:44:28 +0100

    Recent projects section is done.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import RacentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
             ]} />
             <Hero />
             <Grid />
+            <RacentProjects />
         </div>
     </main>
   );
