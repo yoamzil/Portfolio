17137fb849b9e32bccf3e6be4065ad61639bb982
Youness Amzil <amzilyouness@gmail.com>
2024-12-06 20:55:17 +0100

    Added more Navbar items

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import RacentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
-            <FloatingNav navItems={[
-                { name: "Home", link: "/", icon: <FaHome /> },
-            ]} />
+            <FloatingNav navItems={navItems} />
             <Hero />
             <Grid />
             <RacentProjects />
