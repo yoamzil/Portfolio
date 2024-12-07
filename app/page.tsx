63fbf9c42e91a3f876a10031b62677cd34d41e6b
Youness Amzil <amzilyouness@gmail.com>
2024-12-07 01:23:18 +0100

    Added a footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,17 @@ import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import RacentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
             <FloatingNav navItems={navItems} />
             <Hero />
             <Grid />
             <RacentProjects />
+            <Footer />
         </div>
     </main>
   );
