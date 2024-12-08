6f5c3f891bc7471f0901c2f277ada413464041ef
Youness Amzil <amzilyouness@gmail.com>
2024-12-09 00:33:44 +0100

    Added HOME to the navbar

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
+    { name: "Home", link: "#" },
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
   ];
   
