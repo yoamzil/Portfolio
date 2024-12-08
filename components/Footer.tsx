affe962b7e5856e2d2e7a9d922f9171787ca1f59
Youness Amzil <amzilyouness@gmail.com>
2024-12-09 00:55:45 +0100

    Added social media buttons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,9 +23,9 @@ const Footer = () => {
                 <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024</p>
                 <div className='flex items-center md:gap-3 gap-6'>
                     {socialMedia.map((profile) => (
-                        <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
+                        <a key={profile.id} href={profile.link} target="_blank" className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                             <img src={profile.img} alt={profile.id.toString()} width={20} height={20} />
-                        </div>
+                        </a>
                     ))}
                 </div>
             </div>
--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
+      link: "https://github.com/yoamzil",
     },
     {
       id: 2,
       img: "/twit.svg",
+      link: "https://x.com/youness__amzil"
     },
     {
       id: 3,
       img: "/link.svg",
+      link: "https://www.linkedin.com/in/yoamzil/"
     },
   ];
