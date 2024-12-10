db39973d137ac1544869853da13c6c183a0afc4f
Youness Amzil <amzilyouness@gmail.com>
2024-12-10 12:35:24 +0100

    Fixed PROJECTS cards height.

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -22,7 +22,7 @@ const RecentProjects = () => {
                 }) => (
                     <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                         <PinContainer title={link} href={link}>
-                            <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-'>
+                            <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[27rem] h-[30vh] mb-10'>
                                 <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                     <img src="/bg.png" alt="bg-img" />
                                 </div>
