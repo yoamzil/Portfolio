ac6d7946628a51ee12839fb17c48419aa3368c0a
Youness Amzil <amzilyouness@gmail.com>
2025-02-04 22:08:15 +0100

    Almost there...

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-    title: "Youness's Portfolio",
+    title: "Portfolio",
     description: "Modern and minimal portfolio",
 };
 
