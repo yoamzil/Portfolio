9f018521f1ec3b0e2b22229aea63a7f91901de20
Youness Amzil <amzilyouness@gmail.com>
2025-02-14 19:05:26 +0100

    Removing unnecessary stuff

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
                     Ready to take <span className='text-purple'>your</span> digital presence to the next level?
                 </h1>
                 <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
-                <a href='/resume.pdf' download>
+                <a href='/AmzilYouness.pdf' download>
                     <MagicButton
                         title="Download My Resume"
                         icon={<FaDownload />}
