3ef947032b9772e12a28d930bc84fed412018f7c
Youness Amzil <amzilyouness@gmail.com>
2025-02-15 17:08:35 +0100

    Improved the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 const Footer = () => {
     return (
-        <footer className='w-full mb-[100px] pb-10 md:mb-5' id="contact">
+        <footer className='w-full pb-10 md:mb-5' id="contact">
             <div className='flex flex-col items-center'>
                 <h1 className='heading lg:max-w-[45vw]'>
                     Ready to take <span className='text-purple'>your</span> digital presence to the next level?
@@ -20,7 +20,7 @@ const Footer = () => {
                     />
                 </a>
             </div>
-            <div className='flex mt-16 flex-row justify-between items-center'>
+            <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
                 <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2025</p>
                 <div className='flex items-center md:gap-3 gap-6'>
                     {socialMedia.map((profile) => (
