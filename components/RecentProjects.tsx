a93c0d6ae1c61a905b182d41178c4556e1f431be
Youness Amzil <amzilyouness@gmail.com>
2025-02-15 01:28:22 +0100

    Fixed cards rounded corners on small devices

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -23,7 +23,7 @@ const RecentProjects = () => {
                     <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                         <PinContainer title={link} href={link}>
                             <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[27rem] h-[30vh] mb-10'>
-                                <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                                <div className='relative w-full h-full overflow-hidden rounded-3xl bg-[#13162d]'>
                                     <img src="/bg.png" alt="bg-img" />
                                 </div>
                                 <img
